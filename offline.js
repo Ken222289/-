4d5a3ebdc2979649c1ab135ec490f12ac7d072d0
Nino3535 <107600191+Ken222289@users.noreply.github.com>
2022-07-07 13:47:26 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1657176301,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/bulle-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/au-sheet0.png",
+		"images/au2-sheet0.png",
+		"images/box-sheet0.png",
+		"images/เลือด-sheet0.png",
+		"images/play-sheet0.png",
+		"images/bg1-sheet0.png",
+		"images/bg2-sheet0.png",
+		"images/play1-sheet0.png",
+		"images/bg3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
